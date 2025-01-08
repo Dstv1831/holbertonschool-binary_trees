6570e2c86df76a9819d4acac6b27d15f83fb2aa9
DSTV <9871@holbertonstudents.com>
2025-01-08 05:13:25 +0000

    Task Completed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node = malloc(sizeof(binary_tree_t));
 
-	if (new_left_node == NULL || parent == NULL)
+	if (new_left_node == NULL)
+		return (NULL);
+	if (parent == NULL)
 		return (NULL);
 
 	new_left_node->n = value;
