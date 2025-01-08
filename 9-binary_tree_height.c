7c6ebc3a4ff9f4ea342bc22ede3acac843777b52
DSTV <9871@holbertonstudents.com>
2025-01-08 09:17:30 +0000

    Task Completed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_h, right_h;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
