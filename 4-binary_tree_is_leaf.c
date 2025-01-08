a67328ff672793342b83c736c5d979166ecd54b0
DSTV <9871@holbertonstudents.com>
2025-01-08 06:04:05 +0000

    Task Completed

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,11 +10,6 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	
-	if (node->left == NULL && node->right == NULL)
+	if (node && !(node->left || node->right)
 		return (1);
-	else
-		return (0);
 }
