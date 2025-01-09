29d2eab7706e9bc077fdef2da19196667e1133df
DSTV <9871@holbertonstudents.com>
2025-01-09 00:52:00 +0000

    Task Completed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,6 +6,25 @@
  * Return: height of tree
  */
 
+int binary_height(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_height(tree->left);
+	right_height = binary_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ *  binary_tree_balance - function that measures the balance factor
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: If tree is NULL, return 0
+ */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
