f4e24d66e828e9cb5c561da52c4ef33143ae2da3
DSTV <9871@holbertonstudents.com>
2025-01-09 00:22:47 +0000

    Task Completed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (2);
+		return (1);
 
 	left = binary_tree_is_perfect(tree->left);
 	right = binary_tree_is_perfect(tree->right);
