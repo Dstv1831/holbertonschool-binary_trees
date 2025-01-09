0e513a02500ef4d66eac1ee3ecca8beebcc40cfb
DSTV <9871@holbertonstudents.com>
2025-01-09 00:54:04 +0000

    Task Completed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,8 +32,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_height(tree->left);
+	right_height = binary_height(tree->right);
 
 	return (left_height - right_height);
 }
